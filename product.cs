3802e9fb08a93cac01d9ef39ec7c1c58e7df3327
tuyen5tuoi1412 <75303161+tuyen5tuoi1412@users.noreply.github.com>
2021-09-15 17:52:33 +0700

    first commit

---
+++ product.cs
@@ -0,0 +1,15 @@
+using System;
+namespace _13_09_2021
+{
+    public class Student;
+    {
+        public String name;
+        public int age;
+        // constructor 
+        public Student(String name, int age){
+            this.name = name;
+            this.age = age; 
+        }
+    }
+
+}
